63bf3da025d114cd53bdfe61f64bdc6be4f87152
kelli-the-coder <51453786+kelli-the-coder@users.noreply.github.com>
2019-12-29 13:32:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function change_gif() {
-  if (document.getElementById("duckgif").src === "duck.gif") {
-    document.getElementById("duckgif").src = "Baby_Beter.PNG"; 
-  } else {
-    document.getElementById("duckgif").src = "duck.gif";
-  }
+    document.getElementById("duckgif").src = "Baby_Beter.PNG";
 }
