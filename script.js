b34b5c41c3b3995b62c6e771cf2e2e16b87e67bb
Kelli <51453786+kelli-the-coder@users.noreply.github.com>
2020-08-15 21:11:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function change_gif() {
-    document.getElementById("duckgif").src = "Baby_Beter.PNG"; 
-} 
+function change_gif(){
+if (document.getElementById("duckgif").getAttribute('src') == "duck.gif") {
+  document.getElementById("duckgif").src = "Baby_Beter.PNG"; 
+  document.getElementById("changegif").innerText = "Duck"
+}
+else {
+  document.getElementById("duckgif").src = "duck.gif";
+  document.getElementById("changegif").innerText = "Beter"
+}
+}
